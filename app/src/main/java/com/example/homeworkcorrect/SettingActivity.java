da605e9fab9d3a09d2fc8f0e0a8b9473b621fdfb
张子华 <44219201+ZihuaZhang@users.noreply.github.com>
2020-11-23 17:14:36 +0800

    submit

--- app/src/main/java/com/example/homeworkcorrect/SettingActivity.java
+++ app/src/main/java/com/example/homeworkcorrect/SettingActivity.java
@@ -1,8 +1,8 @@
 package com.example.homeworkcorrect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        
     }
 }
