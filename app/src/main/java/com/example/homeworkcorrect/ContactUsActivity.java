406ebc5a5291b251dcd3fa70777636497b4c1f01
张子华 <44219201+ZihuaZhang@users.noreply.github.com>
2020-11-25 12:11:30 +0800

    submit

---
+++ app/src/main/java/com/example/homeworkcorrect/ContactUsActivity.java
@@ -0,0 +1,27 @@
+package com.example.homeworkcorrect;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+        RelativeLayout call = findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18831166710"));
+                startActivity(intent);
+            }
+        });
+    }
+}
