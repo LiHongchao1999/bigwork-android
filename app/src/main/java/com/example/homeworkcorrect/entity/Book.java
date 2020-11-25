d29fd66f03ff726cfd8030956eb454389515292b
LiHongchao <44219658+LiHongchao1999@users.noreply.github.com>
2020-11-25 12:11:10 +0800

    更新商城

---
+++ app/src/main/java/com/example/homeworkcorrect/entity/Book.java
@@ -0,0 +1,92 @@
+package com.example.homeworkcorrect.entity;
+
+public class Book {
+    private int id;
+    private String bookName;//书本名称
+    private String bookAuthor;//书本作者
+    private String bookPress;//书本出版社
+    private double price;//书本价格
+    private int stock;//书本库存
+    private String image;//书本图片
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookPress() {
+        return bookPress;
+    }
+
+    public void setBookPress(String bookPress) {
+        this.bookPress = bookPress;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Book() {
+    }
+
+    public Book(String bookName, String bookAuthor, String bookPress, double price, int stock, String image) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookPress = bookPress;
+        this.price = price;
+        this.stock = stock;
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookPress='" + bookPress + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/homeworkcorrect/fragment/OnlineShopFragment.java
+++ app/src/main/java/com/example/homeworkcorrect/fragment/OnlineShopFragment.java
@@ -1,9 +1,18 @@
 package com.example.homeworkcorrect.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +20,23 @@ import androidx.fragment.app.Fragment;
 
 import com.example.homeworkcorrect.GlideImageLoader;
 import com.example.homeworkcorrect.R;
+import com.example.homeworkcorrect.adapter.CustomBookAdapter;
+import com.example.homeworkcorrect.entity.Book;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.youth.banner.Banner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class OnlineShopFragment extends Fragment {
     private View root;
     private Banner banner;
+    private List<Book> books = new ArrayList<>();
+    private CustomBookAdapter customBookAdapter;
+    private SmartRefreshLayout refreshLayout;
 
     //资源文件
     private Integer[] images = {R.drawable.cake01, R.drawable.cake02, R.drawable.cake03, R.drawable.cake04, R.drawable.cake05};
@@ -30,8 +49,8 @@ public class OnlineShopFragment extends Fragment {
 
         findViews();
         setListeners();
-
-        Banner banner = (Banner) root.findViewById(R.id.banner);
+        getBookList();
+        banner = (Banner) root.findViewById(R.id.banner);
         //设置图片加载器
         banner.setImageLoader(new GlideImageLoader());
         //设置图片集合
@@ -41,10 +60,106 @@ public class OnlineShopFragment extends Fragment {
         return root;
     }
 
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg) {
+            // 处理消息
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    //准备数据
+                    initView();
+                    break;
+                case 2:
+
+                    break;
+            }
+        };
+
+    };
+
+    private void initView() {
+        customBookAdapter = new CustomBookAdapter(getContext(), books,
+                R.layout.book_list_item);
+        GridView bookListView = root.findViewById(R.id.gv_book);
+        bookListView.setAdapter(customBookAdapter);
+        bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //打印
+                Log.e("选中的蛋糕名称----------------","" + books.get(position).getBookName());
+                int menu_id =  books.get(position).getId();
+                //判断用户是否登录
+//                if (user_Name.length() <= 0){
+//                    Toast.makeText(getActivity(), "请先登录",
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//
+//                    //跳转到蛋糕详细信息的Activity
+//                    Intent intent = new Intent();
+//                    //设置目的地Activity类
+//                    intent.setClass(getActivity(),
+//                            ShowCakeInfoActivity.class);
+//                    //传递自定义类型的对象
+//                    //携带用户选中的菜谱id
+//                    intent.putExtra("cake",cakes.get(position));
+//                    startActivity(intent);
+//                }
+            }
+        });
+
+        //setListener();
+    }
+
+
+    private void getBookList() {
+        Book book1 = new Book("计算高手五年级上每日10分钟","晏飞1","河北少年儿童出版社",21.0f,10,"");
+        books.add(book1);
+        Book book2 = new Book("计算高手五年级上每日10分钟","晏飞2","河北少年儿童出版社",22.0f,9,":");
+        books.add(book2);
+        Book book3 = new Book("计算高手五年级上每日10分钟","晏飞3","河北少年儿童出版社",23.0f,8,":");
+        books.add(book3);
+        Book book4 = new Book("计算高手五年级上每日10分钟","晏飞4","河北少年儿童出版社",24.0f,7,":");
+        books.add(book4);
+        Book book5 = new Book("计算高手五年级上每日10分钟","晏飞5","河北少年儿童出版社",25.0f,6,":");
+        books.add(book5);
+        //资源下载完成，返回消息
+        Message msg = handler.obtainMessage();
+        msg.what = 1;
+        handler.sendMessage(msg);
+    }
+
     private void setListeners() {
     }
 
     private void findViews() {
         banner = root.findViewById(R.id.banner);
+    }
+
+    private void setListener(){
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                Log.e("下拉刷新","666");
+                getBookList();
+                //资源下载完成，返回消息
+                Message msg = handler.obtainMessage();
+                msg.what = 3;
+                handler.sendMessage(msg);
+
+            }
+        });
+
+//        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+//            @Override
+//            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+//                Log.e("上拉加载","666");
+//                refreshLayout.finishLoadMore();
+//
+//            }
+//        });
+
+
     }
 }
