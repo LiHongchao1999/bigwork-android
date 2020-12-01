c02385de5206f76043af5741c16cebe79f11543e
irvingding <1826466183@qq.com>
2020-12-01 11:21:46 +0800

    Merge branch 'master' of C:\Users\DELL\Documents\GitHub\bigwork-android with conflicts.

--- app/src/main/java/com/example/homeworkcorrect/LoginActivity.java
+++ app/src/main/java/com/example/homeworkcorrect/LoginActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,17 @@ import com.example.homeworkcorrect.fragment.MyFragmentMainContent;
 
 public class LoginActivity extends AppCompatActivity {
     private Button login;
+
+    String APPKEY = "319a190fd6280";
+    String APPSECRET = "54f8a204b312c31507ed4533104f51ed";
+    //获取用到的控件
+    EditText etphone;
+    EditText etCode;
+    TextView tvlogin;
+    Button btCode;
+
+    private int i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
