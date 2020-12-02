26361a15380f6b3fe4228576dfb74cb4d9499408
张子华 <44219201+ZihuaZhang@users.noreply.github.com>
2020-12-02 12:23:56 +0800

    submit

---
+++ app/src/main/java/com/example/homeworkcorrect/CommonUtil.java
@@ -0,0 +1,22 @@
+package com.example.homeworkcorrect;
+
+import android.content.Context;
+
+public class CommonUtil {
+    public static int dp2px(Context context, float dpValue) {
+        if (null == context) {
+            return 0;
+        }
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+//...
+
+    public static int getScreenWidth(Context context) {
+        if (null == context) {
+            return 0;
+        }
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+}
--- app/src/main/java/com/example/homeworkcorrect/fragment/OnlineShopFragment.java
+++ app/src/main/java/com/example/homeworkcorrect/fragment/OnlineShopFragment.java
@@ -9,16 +9,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.homeworkcorrect.AnimationNestedScrollView;
+import com.example.homeworkcorrect.CommonUtil;
 import com.example.homeworkcorrect.GlideImageLoader;
 import com.example.homeworkcorrect.R;
 import com.example.homeworkcorrect.ScrollableGridView;
@@ -48,9 +49,13 @@ public class OnlineShopFragment extends Fragment {
     private SmartRefreshLayout refreshLayout;
     private TextView tv_book_store;
     private TextView tv_class;
-
+    private AnimationNestedScrollView sv_view;
+    private LinearLayout ll_search;
+    private TextView tv_title;
+    private float LL_SEARCH_MIN_TOP_MARGIN, LL_SEARCH_MAX_TOP_MARGIN, LL_SEARCH_MAX_WIDTH, LL_SEARCH_MIN_WIDTH, TV_TITLE_MAX_TOP_MARGIN;
+    private ViewGroup.MarginLayoutParams searchLayoutParams, titleLayoutParams;
     //资源文件
-    private Integer[] images = {R.drawable.cake01, R.drawable.cake02, R.drawable.cake03, R.drawable.cake04, R.drawable.cake05};
+    private Integer[] images = {R.drawable.inclass1, R.drawable.cake02, R.drawable.cake03, R.drawable.cake04, R.drawable.cake05};
 
 
     @Nullable
@@ -104,6 +109,17 @@ public class OnlineShopFragment extends Fragment {
         customBookAdapter = new CustomBookAdapter(getContext(), books,
                 R.layout.book_list_item);
         ScrollableGridView bookListView = root.findViewById(R.id.gv_book);
+        bookListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+            }
+        });
         bookListView.setAdapter(customBookAdapter);
         bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -192,6 +208,52 @@ public class OnlineShopFragment extends Fragment {
         banner = root.findViewById(R.id.banner);
         tv_book_store = root.findViewById(R.id.tv_book_store);
         tv_class = root.findViewById(R.id.tv_class);
+        sv_view = root.findViewById(R.id.search_sv_view);
+        ll_search = root.findViewById(R.id.search_ll_search);
+        tv_title = root.findViewById(R.id.search_tv_title);
+        searchLayoutParams = (ViewGroup.MarginLayoutParams) ll_search.getLayoutParams();
+        titleLayoutParams = (ViewGroup.MarginLayoutParams) tv_title.getLayoutParams();
+
+        LL_SEARCH_MIN_TOP_MARGIN = CommonUtil.dp2px(getContext(), 10f);//布局关闭时顶部距离
+        LL_SEARCH_MAX_TOP_MARGIN = CommonUtil.dp2px(getContext(), 49f);//布局默认展开时顶部距离
+        LL_SEARCH_MAX_WIDTH = CommonUtil.getScreenWidth(getContext()) - CommonUtil.dp2px(getContext(), 30f);//布局默认展开时的宽度
+        LL_SEARCH_MIN_WIDTH = CommonUtil.getScreenWidth(getContext()) - CommonUtil.dp2px(getContext(), 82f);//布局关闭时的宽度
+        TV_TITLE_MAX_TOP_MARGIN = CommonUtil.dp2px(getContext(), 11.5f);
+
+        sv_view.setOnAnimationScrollListener(new AnimationNestedScrollView.OnAnimationScrollChangeListener() {
+            @Override
+            public void onScrollChanged(float dy) {
+                float searchLayoutNewTopMargin = LL_SEARCH_MAX_TOP_MARGIN - dy;
+                float searchLayoutNewWidth = LL_SEARCH_MAX_WIDTH - dy * 1.3f;//此处 * 1.3f 可以设置搜索框宽度缩放的速率
+
+                float titleNewTopMargin = (float) (TV_TITLE_MAX_TOP_MARGIN - dy * 0.5);
+
+                //处理布局的边界问题
+                searchLayoutNewWidth = searchLayoutNewWidth < LL_SEARCH_MIN_WIDTH ? LL_SEARCH_MIN_WIDTH : searchLayoutNewWidth;
+
+                if (searchLayoutNewTopMargin < LL_SEARCH_MIN_TOP_MARGIN) {
+                    searchLayoutNewTopMargin = LL_SEARCH_MIN_TOP_MARGIN;
+                }
+
+                if (searchLayoutNewWidth < LL_SEARCH_MIN_WIDTH) {
+                    searchLayoutNewWidth = LL_SEARCH_MIN_WIDTH;
+                }
+
+                float titleAlpha = 255 * titleNewTopMargin / TV_TITLE_MAX_TOP_MARGIN;
+                if (titleAlpha < 0) {
+                    titleAlpha = 0;
+                }
+
+                //设置相关控件的LayoutParams  此处使用的是MarginLayoutParams，便于设置params的topMargin属性
+                tv_title.setTextColor(tv_title.getTextColors().withAlpha((int) titleAlpha));
+                titleLayoutParams.topMargin = (int) titleNewTopMargin;
+                tv_title.setLayoutParams(titleLayoutParams);
+
+                searchLayoutParams.topMargin = (int) searchLayoutNewTopMargin;
+                searchLayoutParams.width = (int) searchLayoutNewWidth;
+                ll_search.setLayoutParams(searchLayoutParams);
+            }
+        });
     }
 
     private void setListener() {
