bb4dea580a7c03a8e7ebb156764364e7bac36575
李宏超 <44219658+LiHongchao1999@users.noreply.github.com>
2020-12-03 11:40:32 +0800

    更新user类

--- app/src/main/java/com/example/homeworkcorrect/entity/User.java
+++ app/src/main/java/com/example/homeworkcorrect/entity/User.java
@@ -1,33 +1,80 @@
 package com.example.homeworkcorrect.entity;
 
-import android.graphics.Bitmap;
 
 public class User {
-    private String nickName;//昵称
+    private int id;//用户id
+    private String nickname;//用户昵称
+    private String phoneNumber;//手机号码
+    private String password;//密码
+    private String image;//头像
+    private String qqNumber;//QQ号码
+    private String weChatNumber;//微信号码
+    private String grade;//年级
     private String sex;//性别
-    private Bitmap img;//用户头像
 
-    public Bitmap getImg() {
-        return img;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
-    public void setImg(Bitmap img) {
-        this.img = img;
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getQqNumber() {
+        return qqNumber;
+    }
+    public void setQqNumber(String qqNumber) {
+        this.qqNumber = qqNumber;
+    }
+    public String getWeChatNumber() {
+        return weChatNumber;
+    }
+    public void setWeChatNumber(String weChatNumber) {
+        this.weChatNumber = weChatNumber;
+    }
+    public String getGrade() {
+        return grade;
+    }
+    public void setGrade(String grade) {
+        this.grade = grade;
     }
 
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
 
     public String getSex() {
         return sex;
     }
-
     public void setSex(String sex) {
         this.sex = sex;
     }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", nickname=" + nickname + ", phoneNumber=" + phoneNumber + ", password=" + password
+                + ", image=" + image + ", qqNumber=" + qqNumber + ", weChatNumber=" + weChatNumber + ", grade=" + grade
+                + ", sex=" + sex + "]";
+    }
+    
 }
